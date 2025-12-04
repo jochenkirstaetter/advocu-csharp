26519e7f8088cb85192e74cefcb830fb225f0cd9
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-12-05 02:07:16 +0400

    sort enum member values alphabetically

--- src/Advocu.WebApp/AdvocuDataService.cs
+++ src/Advocu.WebApp/AdvocuDataService.cs
@@ -1,9 +1,13 @@
+using Advocu.WebApp.Extensions;
+
 namespace Advocu.WebApp;
 
 public class AdvocuDataService
 {
     public List<AdvocuTag> GetTags()
     {
-        return Enum.GetValues<AdvocuTag>().ToList();
+        return Enum.GetValues<AdvocuTag>()
+            .OrderBy(t => t.GetDisplayName())
+            .ToList();
     }
 }
