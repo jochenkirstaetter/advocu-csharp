ec682e1f3319646c8883a6d1932ed46a854b9525
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-12-22 13:17:49 +0400

    add environments to code and report

---
+++ .idx/dev.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }: {
+
+  # Which nixpkgs channel to use.
+  channel = "stable-25.05"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.dotnetCorePackages.sdk_10_0
+    pkgs.dotnet-sdk_10
+  ];
+
+  # Sets environment variables in the workspace
+  env = {
+    DOTNET_NOLOGO = "true";
+    DOTNET_CLI_TELEMETRY_OPTOUT = "true";
+    DOTNET_SKIP_FIRST_TIME_EXPERIENCE = "true";
+    DOTNET_SYSTEM_GLOBALIZATION_INVARIANT = "1";
+  };
+
+  # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+  idx.extensions = [
+    "muhammad-sammy.csharp"
+    "ms-azuretools.vscode-docker"
+    "humao.rest-client"
+    "rangav.vscode-thunder-client"
+  ];
+
+  # enterShell = ''
+  #   echo "Checks latest info in the README.md."
+  #   echo "The tests provide the latest and greatest samples."
+  # '';
+}
